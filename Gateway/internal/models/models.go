e58172c08de3e6beca30a59d53d5a9d871b31c30
alexey <molokanov-oleksiy@yandex.ru>
2025-05-01 17:19:51 +0300

    fix GetMe

--- Gateway/internal/models/models.go
+++ Gateway/internal/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type RegisterRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -22,10 +24,12 @@ type LoginResponse struct {
 type GetMeRequest struct {
 }
 type GetMeResponse struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
+	ID       string    `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Email    string    `json:"email"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
 }
 
 type WSInput struct {
--- Gateway/internal/repository/user.go
+++ Gateway/internal/repository/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"gitlab.com/bobr-lord-messenger/gateway/internal/config"
+	"gitlab.com/bobr-lord-messenger/gateway/internal/errors"
 	"gitlab.com/bobr-lord-messenger/gateway/internal/models"
 	"io"
 	"net/http"
@@ -20,27 +21,30 @@ func NewUserRepository(cfg *config.Config) *UserRepository {
 func (r *UserRepository) GetMe(id string) (*models.GetMeResponse, error) {
 	req, err := http.NewRequest("GET", "http://"+r.cfg.UserServiceHost+":"+r.cfg.UserServicePort+"/me", nil)
 	if err != nil {
-		return nil, fmt.Errorf("could not create request: %w", err)
+		return nil, errors.NewCustomError(http.StatusInternalServerError, fmt.Sprintf("could not create request: %w", err))
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("id", id)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("could not send request: %w", err)
+		return nil, errors.NewCustomError(http.StatusInternalServerError, fmt.Sprintf("could not send request: %w", err))
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("could not read response: %w", err)
+		return nil, errors.NewCustomError(http.StatusInternalServerError, fmt.Sprintf("could not read response: %w", err))
 	}
 
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, errors.NewCustomError(http.StatusNotFound, "user not found")
+	}
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("could not get user: %s", body)
+		return nil, errors.NewCustomError(resp.StatusCode, string(body))
 	}
 	var user models.GetMeResponse
 	err = json.Unmarshal(body, &user)
 	if err != nil {
-		return nil, fmt.Errorf("could not unmarshal response: %w", err)
+		return nil, errors.NewCustomError(http.StatusInternalServerError, fmt.Sprintf("could not unmarshal response: %w", err))
 	}
 	return &user, nil
 }
